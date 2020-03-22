7bca5bccaa6ae98620fc08e272f4691f800cba1f
João Vitor Medeiros <joao.vitor@buildbox.com.br>
2020-03-22 00:29:23 -0300

    Updates

---
+++ precache-manifest.9987db796eb55658237c321aa52b7780.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c554dd29dea52d65f6d61a5ff37d7166",
+    "url": "/index.html"
+  },
+  {
+    "revision": "152e758d74ad9c37c67c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1475b0f261777fbc661c",
+    "url": "/static/js/2.27da7799.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27da7799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152e758d74ad9c37c67c",
+    "url": "/static/js/main.2c144201.chunk.js"
+  },
+  {
+    "revision": "7da55441ac457370fc07",
+    "url": "/static/js/runtime-main.65bf00d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
